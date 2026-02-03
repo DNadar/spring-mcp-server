c69dc25a1607416d086ad1ee3766eb8682c05810
Domnic Nadar <Domnic.Nadar@guitarcenter.com>
2026-02-03 12:23:26 -0800

    added security for okta

--- src/main/java/com/gc/mcp/server/config/SecurityConfig.java
+++ src/main/java/com/gc/mcp/server/config/SecurityConfig.java
@@ -1,19 +1,53 @@
 package com.gc.mcp.server.config;
 
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
 
+
+    private final String issuer;
+    private final String jwkSetUri;
+
+    public SecurityConfig(
+            @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}") String issuer,
+            @Value("${jwt.jwk-set-uri}") String jwkSetUri
+    ) {
+        this.issuer = issuer;
+        this.jwkSetUri = jwkSetUri;
+    }
+
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        NimbusReactiveJwtDecoder decoder = NimbusReactiveJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+
+        JwtTypeValidator typ = new JwtTypeValidator("JWT", "application/okta-internal-at+jwt");
+        //typ.setAllowEmpty(true); // allow missing typ too, if you want
+
+        decoder.setJwtValidator(JwtValidators.createDefaultWithValidators(
+                new JwtTimestampValidator(),
+                new JwtIssuerValidator(this.issuer),
+                typ
+        ));
+
+        return decoder;
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http,
+                                                         ReactiveJwtDecoder reactiveJwtDecoder) {
         var successHandler = new RedirectServerAuthenticationSuccessHandler("/connected");
 
         http
@@ -25,8 +59,11 @@ public class SecurityConfig {
                 )
                 .oauth2Login(oauth2 -> oauth2.authenticationSuccessHandler(successHandler))
                 .oauth2Client(Customizer.withDefaults())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtDecoder(reactiveJwtDecoder))   // <-- key line
+                );
 
         return http.build();
     }
+
 }
